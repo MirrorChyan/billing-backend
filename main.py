cab7992b1801855ef9b2c01ee677fbee543f4561
MistEO <mistereo@hotmail.com>
2025-01-16 14:39:16 +0800

    fix: typo

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from src.order.afdian.webhook import router as order_afdian_webhook_router
 from src.order.afdian.query_order import router as order_afdian_query_order_router
-from scr.check_in import router as check_in_router
+from src.check_in import router as check_in_router
 
 app = FastAPI()
 
