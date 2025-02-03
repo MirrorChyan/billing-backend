adde3d947ab508422a6f784fb65b55399343d536
MistEO <mistereo@hotmail.com>
2025-02-04 05:11:53 +0800

    chore: 优化查账小工具 (#10)

--- check_bills.py
+++ check_bills.py
@@ -1,6 +1,13 @@
 from datetime import datetime, timedelta
+from pathlib import Path
+import json
 
 from src.database import Bill, Plan, CheckIn
+
+
+def secure_str(s: str) -> str:
+    half = len(s) // 2
+    return s[:half] + "*" * (len(s) - half)
 
 
 def monthly_bill(year: int, month: int):
@@ -16,12 +23,25 @@ def monthly_bill(year: int, month: int):
     all_plans = Plan.select().order_by(Plan.valid_days)
     plans = {}
     for plan in all_plans:
-        plans[plan.plan_id] = (plan.title, 0, 0) # name, order, item
+        plans[plan.plan_id] = (plan.title, 0, 0)  # name, order, item
 
     order_count = 0
     item_count = 0
     bill_amount = 0
+    order_csv = "order_id,plan,buy_count,amount,remark,user_id,created_at,expired_at\n"
+    remark_csv = order_csv
     for bill in bills:
+        remark = (
+            json.loads(bill.raw_data)
+            .get("data", {})
+            .get("list", [{}])[0]
+            .get("remark", "")
+        )
+        csv_line = f"{secure_str(bill.order_id)},{plans[bill.plan_id][0]},{bill.buy_count},{bill.actually_paid},{remark},{secure_str(bill.user_id)},{bill.created_at},{bill.expired_at}\n"
+        order_csv += csv_line
+        if remark:
+            remark_csv += csv_line
+
         if bill.plan_id not in plans:
             print(f"Plan not found: {bill.plan_id}, bill: {bill}")
             continue
@@ -39,21 +59,54 @@ def monthly_bill(year: int, month: int):
     for plan_id, (name, order, item) in plans.items():
         print(f"{name}: {order} orders, {item} items")
 
-    print(f"\nTotal: orders: {order_count}, items: {item_count}, amount: {bill_amount:.2f}")
+    print(
+        f"\nTotal: orders: {order_count}, items: {item_count}, amount: {bill_amount:.2f}"
+    )
+
+    Path(f"csv/{year}-{month}").mkdir(parents=True, exist_ok=True)
+    with open(
+        f"csv/{year}-{month}/{year}-{month} orders.csv", "w", encoding="utf-8"
+    ) as f:
+        f.write("\ufeff")  # BOM
+        f.write(order_csv)
+    print(f"CSV saved: {year}-{month} orders.csv")
+
+    with open(
+        f"csv/{year}-{month}/{year}-{month} remarks.csv", "w", encoding="utf-8"
+    ) as f:
+        f.write("\ufeff")  # BOM
+        f.write(remark_csv)
+    print(f"CSV saved: {year}-{month} remarks.csv")
 
     print("\n==== Checkins ====\n")
-
-    app_uas = {}
 
     checkins = CheckIn.select().where(
         CheckIn.activated_at.year == year,
         CheckIn.activated_at.month == month,
     )
 
-    # 有可能订单在上个月，但是激活在这个月
+    # 有可能订单是以前的，但是激活是在这个月的
+    if month == 12:
+        next_month = datetime(year + 1, 1, 1)
+    else:
+        next_month = datetime(year, month + 1, 1)
+
+    # 但也别太久远了，最多三个月前
+    if month <= 3:
+        prev_month = datetime(year - 1, 12 + month - 3, 1)
+    else:
+        prev_month = datetime(year, month - 3, 1)
+
+    plan_titles = {plan.plan_id: plan.title for plan in all_plans}
+
     bills = Bill.select().where(
-        Bill.created_at < datetime(year, month, 1) + timedelta(days=31),
+        Bill.created_at < next_month,
+        Bill.created_at >= prev_month,
     )
+
+    app_uas = {}
+    csvs = {}
+    csv_head = "order_id,plan,buy_count,amount,remark,user_id,created_at,expired_at,activated_at\n"
 
     invalid_checkins = 0
     for checkin in checkins:
@@ -61,7 +114,8 @@ def monthly_bill(year: int, month: int):
         ua = checkin.user_agent
         app_ua = f"{app} - {ua}"
         if app_ua not in app_uas:
-            app_uas[app_ua] = (0, 0) # count, amount
+            app_uas[app_ua] = (0, 0)  # count, amount
+            csvs[app_ua] = csv_head
 
         count = 0
         amount = 0
@@ -69,6 +123,15 @@ def monthly_bill(year: int, month: int):
             if bill.cdk == checkin.cdk:
                 count += 1
                 amount += float(bill.actually_paid)
+                remark = (
+                    json.loads(bill.raw_data)
+                    .get("data", {})
+                    .get("list", [{}])[0]
+                    .get("remark", "")
+                )
+                csvs[
+                    app_ua
+                ] += f"{secure_str(bill.order_id)},{plan_titles[bill.plan_id]},{bill.buy_count},{bill.actually_paid},{remark},{secure_str(bill.user_id)},{bill.created_at},{bill.expired_at},{checkin.activated_at}\n"
 
         if count == 0:
             invalid_checkins += 1
@@ -90,9 +153,30 @@ def monthly_bill(year: int, month: int):
 
     not_checkin_count = order_count - checkin_count
     not_checkin_amount = bill_amount - checkin_amount
-    print(f"\nOrders not checkin: {not_checkin_count}, amount: {not_checkin_amount:.2f}")
+    print(
+        f"\nOrders not checkin: {not_checkin_count}, amount: {not_checkin_amount:.2f}\n"
+    )
 
+    Path(f"csv/{year}-{month}").mkdir(parents=True, exist_ok=True)
+    for app_ua, csv in csvs.items():
+        with open(
+            f"csv/{year}-{month}/{year}-{month} {app_ua}.csv", "w", encoding="utf-8"
+        ) as f:
+            f.write("\ufeff")  # BOM
+            f.write(csv)
+        print(f"CSV saved: {year}-{month} {app_ua}.csv")
 
 
 if __name__ == "__main__":
-    monthly_bill(datetime.now().year, datetime.now().month)
+    now = datetime.now()
+
+    def pre_month(now):
+        year = now.year
+        month = now.month - 1
+        if month == 0:
+            year -= 1
+            month = 12
+        return year, month
+
+    # monthly_bill(*pre_month(now))
+    monthly_bill(now.year, now.month)
